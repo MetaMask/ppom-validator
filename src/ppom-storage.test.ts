4352523e2d503a41c749938c32e9f18d3ac51ef2
Jyoti Puri <jyotipuri@gmail.com>
2023-05-29 11:18:16 +0530

    Update src/ppom-storage.test.ts
    Co-authored-by: Elliot Winkler <elliot.winkler@gmail.com>

--- src/ppom-storage.test.ts
+++ src/ppom-storage.test.ts
@@ -211,7 +211,7 @@ describe('PPOMStorage', () => {
     });
   });
 
-  describe('PPOMStorage:syncMetadata', () => {
+  describe('syncMetadata', () => {
     it('should return metadata of file if updated file is found in storage', async () => {
       const fileData = {
         chainId: '1',
