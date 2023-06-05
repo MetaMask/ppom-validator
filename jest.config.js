00964f52b89fde1607b717492cf76796de930b22
Jyoti Puri <jyotipuri@gmail.com>
2023-06-05 15:05:25 +0530

    Update

--- jest.config.js
+++ jest.config.js
@@ -161,7 +161,9 @@ module.exports = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ['src/ppom.ts'],
+  // testPathIgnorePatterns: [
+  //   "/node_modules/"
+  // ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
