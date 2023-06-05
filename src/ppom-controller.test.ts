3ee76406945c0a39db18f7e6142daf2e741c5a34
Jyoti Puri <jyotipuri@gmail.com>
2023-06-05 17:47:38 +0530

    Fix

--- src/ppom-controller.test.ts
+++ src/ppom-controller.test.ts
@@ -7,7 +7,7 @@ Object.defineProperty(globalThis, 'fetch', {
   value: () => undefined,
 });
 
-jest.mock('./ppom.js', () => ({
+jest.mock('./ppom.ts', () => ({
   PPOM: class PPOMClass {
     #jsonRpcRequest;
 
