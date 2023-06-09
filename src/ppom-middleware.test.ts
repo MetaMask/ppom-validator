39b666e5b86cddd3f0bad2cd24a6cafc7d9f8691
Jyoti Puri <jyotipuri@gmail.com>
2023-06-09 14:21:15 +0530

    Updates

--- src/ppom-middleware.test.ts
+++ src/ppom-middleware.test.ts
@@ -20,80 +20,88 @@ jest.mock('./ppom.ts', () => ({
   ppomInit: () => undefined,
 }));
 
-describe('createPPOMMiddleware', () => {
-  it('should return PPOM Middleware function', () => {
+describe('PPOMMiddleware', () => {
+  it('should return PPOM Middleware when createPPOMMiddleware function called', () => {
     const ppomController = buildPPOMController();
     const middlewareFunction = createPPOMMiddleware(ppomController);
     expect(middlewareFunction).toBeDefined();
   });
 
-  describe('PPOMMiddleware', () => {
-    it('should call ppomController.use when invoked', async () => {
-      const useMock = jest.fn();
-      const controller = {
-        usePPOM: useMock,
-      };
-      const middlewareFunction = createPPOMMiddleware(controller as any);
-      await middlewareFunction(
-        { method: 'eth_sendTransaction' },
-        undefined,
-        () => undefined,
-      );
-      expect(useMock).toHaveBeenCalledTimes(1);
-    });
+  it('should call ppomController.usePPOM for requests of type confirmation', async () => {
+    const useMock = jest.fn();
+    const controller = {
+      usePPOM: useMock,
+    };
+    const middlewareFunction = createPPOMMiddleware(controller as any);
+    await middlewareFunction(
+      { method: 'eth_sendTransaction' },
+      undefined,
+      () => undefined,
+    );
+    expect(useMock).toHaveBeenCalledTimes(1);
+  });
 
-    it('should call next method when ppomController.use completes', async () => {
-      const ppom = {
-        validateJsonRpc: () => undefined,
-      };
-      const controller = {
-        usePPOM: async (callback: any) => {
-          callback(ppom);
-        },
-      };
-      const middlewareFunction = createPPOMMiddleware(controller as any);
-      const nextMock = jest.fn();
-      await middlewareFunction(
-        { method: 'eth_sendTransaction' },
-        undefined,
-        nextMock,
-      );
-      expect(nextMock).toHaveBeenCalledTimes(1);
-    });
+  it('should add validation response on confirmation requests', async () => {
+    const controller = {
+      usePPOM: async () => Promise.resolve('VALIDATION_RESULT'),
+    };
+    const middlewareFunction = createPPOMMiddleware(controller as any);
+    const req = { method: 'eth_sendTransaction', ppomResponse: undefined };
+    await middlewareFunction(req, undefined, () => undefined);
+    expect(req.ppomResponse).toBeDefined();
+  });
 
-    it('should call next method when ppomController.use throws error', async () => {
-      const controller = {
-        usePPOM: async (_callback: any) => {
-          throw Error('Some error');
-        },
-      };
-      const middlewareFunction = createPPOMMiddleware(controller as any);
-      const nextMock = jest.fn();
-      await middlewareFunction(
-        { method: 'eth_sendTransaction' },
-        undefined,
-        nextMock,
-      );
-      expect(nextMock).toHaveBeenCalledTimes(1);
-    });
+  it('should call next method when ppomController.usePPOM completes', async () => {
+    const ppom = {
+      validateJsonRpc: () => undefined,
+    };
+    const controller = {
+      usePPOM: async (callback: any) => {
+        callback(ppom);
+      },
+    };
+    const middlewareFunction = createPPOMMiddleware(controller as any);
+    const nextMock = jest.fn();
+    await middlewareFunction(
+      { method: 'eth_sendTransaction' },
+      undefined,
+      nextMock,
+    );
+    expect(nextMock).toHaveBeenCalledTimes(1);
+  });
 
-    it('should call ppom.validateJsonRpc when invoked', async () => {
-      const validateMock = jest.fn();
-      const ppom = {
-        validateJsonRpc: validateMock,
-      };
-      const controller = {
-        usePPOM: async (callback: any) => {
-          callback(ppom);
-        },
-      };
-      const middlewareFunction = createPPOMMiddleware(controller as any);
-      await middlewareFunction(
-        { method: 'eth_sendTransaction' },
-        undefined,
-        () => undefined,
-      );
-      expect(validateMock).toHaveBeenCalledTimes(1);
-    });
+  it('should call next method when ppomController.usePPOM throws error', async () => {
+    const controller = {
+      usePPOM: async (_callback: any) => {
+        throw Error('Some error');
+      },
+    };
+    const middlewareFunction = createPPOMMiddleware(controller as any);
+    const nextMock = jest.fn();
+    await middlewareFunction(
+      { method: 'eth_sendTransaction' },
+      undefined,
+      nextMock,
+    );
+    expect(nextMock).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call ppom.validateJsonRpc when invoked', async () => {
+    const validateMock = jest.fn();
+    const ppom = {
+      validateJsonRpc: validateMock,
+    };
+    const controller = {
+      usePPOM: async (callback: any) => {
+        callback(ppom);
+      },
+    };
+    const middlewareFunction = createPPOMMiddleware(controller as any);
+    await middlewareFunction(
+      { method: 'eth_sendTransaction' },
+      undefined,
+      () => undefined,
+    );
+    expect(validateMock).toHaveBeenCalledTimes(1);
   });
 });
