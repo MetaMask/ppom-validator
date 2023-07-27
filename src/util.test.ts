cb398d150ce0f509a08471450fc0a5f64dd13a99
Jyoti Puri <jyotipuri@gmail.com>
2023-07-27 16:20:16 +0530

    fix

--- src/util.test.ts
+++ src/util.test.ts
@@ -21,30 +21,16 @@ describe('Util', () => {
       );
     });
 
-    it('should validate correct signature', async () => {
-      expect(async () => {
-        const blobData = await fs.promises.readFile('./test/0.0.2');
-        await validateSignature(
-          blobData,
-          'f8b1e03065036fc344c01c172293ed70ba772c56f9e8a2bea8dac0189d6d71b0189c8aa94ed82537cf5deddbbaeaadefbffbbb7a8bfd91fe185b423e6c01190b',
-          '066ad3e8af5583385e312c156d238055215d5f25247c1e91055afa756cb98a88',
-          'valid_data_file',
-        );
-      }).not.toThrow(
-        'Signature verification failed for file path: valid_data_file',
-      );
-    });
-
     it('should validate correct signature - failing', async () => {
-      await expect(async () => {
+      expect(async () => {
         const blobData = await fs.promises.readFile('./test/stale_tags.bin');
         await validateSignature(
           blobData,
           'd56e247e6f5d5033c36ae65f70aa7b0c4a42385eeef63da3af1ca8da28dce0788f45491771acea46fcb1242297231ff9aa59687c53a2b86d498496351b170004',
-          TEST_PUBLIC_KEY,
+          '821e94d60bf030d7f5c399f751324093363a229acc1aa77cfbd795a0e62ff947',
           'valid_data_file',
         );
-      }).rejects.toThrow(
+      }).not.toThrow(
         'Signature verification failed for file path: valid_data_file',
       );
     });
