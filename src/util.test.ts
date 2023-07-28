1cffface71bbb741734c42fb600b197ac5243b3c
Jyoti Puri <jyotipuri@gmail.com>
2023-07-28 17:26:35 +0530

    Update

--- src/util.test.ts
+++ src/util.test.ts
@@ -3,7 +3,10 @@ import fs from 'fs';
 import { validateSignature } from './util';
 
 const TEST_PUBLIC_KEY =
-  '066ad3e8af5583385e312c156d238055215d5f25247c1e91055afa756cb98a88';
+  '821e94d60bf030d7f5c399f751324093363a229acc1aa77cfbd795a0e62ff947';
+const CORRECT_SIGNATURE =
+  'd56e247e6f5d5033c36ae65f70aa7b0c4a42385eeef63da3af1ca8da28dce0788f45491771acea46fcb1242297231ff9aa59687c53a2b86d498496351b170004';
+const INCORRECT_SIGNATURE = 'd56e247e6f5d50a59687c53a2b86d498496351b170004';
 
 describe('Util', () => {
   describe('validateSignature', () => {
@@ -12,7 +15,7 @@ describe('Util', () => {
         const blobData = await fs.promises.readFile('./test/stale_tags.bin');
         await validateSignature(
           blobData,
-          'd56e247e6f5d50a59687c53a2b86d498496351b170004',
+          INCORRECT_SIGNATURE,
           TEST_PUBLIC_KEY,
           'invalid_data_file',
         );
@@ -26,8 +29,8 @@ describe('Util', () => {
         const blobData = await fs.promises.readFile('./test/stale_tags.bin');
         await validateSignature(
           blobData,
-          'd56e247e6f5d5033c36ae65f70aa7b0c4a42385eeef63da3af1ca8da28dce0788f45491771acea46fcb1242297231ff9aa59687c53a2b86d498496351b170004',
-          '821e94d60bf030d7f5c399f751324093363a229acc1aa77cfbd795a0e62ff947',
+          CORRECT_SIGNATURE,
+          TEST_PUBLIC_KEY,
           'valid_data_file',
         );
       }).not.toThrow(
