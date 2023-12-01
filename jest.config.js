42fcfb8e4c2ccecc3969052da57d18d144d488ee
Jyoti Puri <jyotipuri@gmail.com>
2023-12-01 21:49:45 +0530

    Update

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,3 @@
-const crypto = require('crypto');
-
 /*
  * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/configuration
@@ -66,9 +64,6 @@ module.exports = {
   // A set of global variables that need to be available in all test environments
   // globals: {},
 
-  globals: {
-    crypto,
-  },
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   // maxWorkers: "50%",
 
